dc25c722101b758f2edbbf3c3bfb847b7bfbe3ca
Isaiah A <isaiah.althouse@gmail.com>
2022-05-25 23:17:56 -0700

    Reorganize layout to taste

---
+++ components/UnderConstruction/UnderConstruction.tsx
@@ -0,0 +1,13 @@
+import styles from './UnderConstruction.module.css';
+
+const UnderConstruction = () => {
+  return (
+    <div className={styles.container}>
+      <em>
+        Under construction!
+      </em>
+    </div>
+  );
+}
+
+export default UnderConstruction;
