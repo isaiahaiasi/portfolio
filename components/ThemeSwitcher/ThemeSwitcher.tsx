cd122350cae3cd02fea9ad045dd56a00d9615887
Isaiah A <isaiah.althouse@gmail.com>
2022-06-01 11:48:13 -0600

    Change theme switcher input to a radio group

--- components/ThemeSwitcher/ThemeSwitcher.tsx
+++ components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,14 +3,37 @@ import { useThemeSwitcher } from "../../hooks/useThemeSwitcher";
 // TODO:
 // - Create a contextProvider for the themeSwitcher hook at the top level
 // - Avoid "flash" by injecting script?
-
+// - Save preference in local storage
 const ThemeSwitcher = () => {
   const { setTheme, currentTheme, themes } = useThemeSwitcher();
 
   return (
     <div>
-      <button onClick={() => setTheme("dark")}>🌘</button>
-      <button onClick={() => setTheme("light")}>🌞</button>
+      <fieldset>
+        <legend>Select a theme:</legend>
+        <div>
+          <input
+            type="radio"
+            id="theme-option-light"
+            name="theme-select"
+            value="light"
+            checked={"light"===currentTheme}
+            onSelect={() => setTheme("light")}
+          />
+          <label htmlFor="theme-option-light">Light</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="theme-option-dark"
+            name="theme-select"
+            value="dark"
+            checked={"dark"===currentTheme}
+            onSelect={() => setTheme("dark")}
+          />
+          <label htmlFor="theme-option-dark">Dark</label>
+        </div>
+      </fieldset>
       <p>Current theme is: {themes[themes.indexOf(currentTheme)]}. Isn&apos;t it pretty?? 😍</p>
     </div>
   );
